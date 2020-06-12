5113adf9091352c997f06eba7c1591406d6b76b6
proszko <peterzonta97@gmail.com>
2020-06-12 14:56:44 +0200

    requirements updated, working placeholder app created

--- Raport_App/gui.py
+++ Raport_App/gui.py
@@ -1,12 +1,23 @@
 import sys
 import pymysql
 pymysql.install_as_MySQLdb()
+import MySQLdb
 
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 if __name__ == "__main__":
+
+    db = MySQLdb.connect("localhost","root","tYPSa5q74XhGDn5g","database_test" )
+    cursor = db.cursor()
+    cursor.execute("SELECT VERSION()")
+    data = cursor.fetchone()
+    print(data)
+    db.close()
+
     app = QApplication([])
     label = QLabel('Hello World!')
     label.show()
     app.exec_()
+
+
