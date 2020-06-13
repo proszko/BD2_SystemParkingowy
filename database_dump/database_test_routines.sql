d7908341052bf3ff91c19f425ac7ab6fd358d702
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2020-06-13 15:41:26 +0200

    more data to db dump added

--- database_dump/database_test_routines.sql
+++ database_dump/database_test_routines.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `database_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `database_test`;
 -- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
 -- Host: localhost    Database: database_test
@@ -16,28 +18,8 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `typy_pojazdów`
+-- Dumping routines for database 'database_test'
 --
-
-DROP TABLE IF EXISTS `typy_pojazdów`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `typy_pojazdów` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nazwa` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `typy_pojazdów`
---
-
-LOCK TABLES `typy_pojazdów` WRITE;
-/*!40000 ALTER TABLE `typy_pojazdów` DISABLE KEYS */;
-INSERT INTO `typy_pojazdów` VALUES (1,'osobowy'),(2,'ciężarowy'),(3,'autobus'),(4,'taksówka'),(5,'jednoślad');
-/*!40000 ALTER TABLE `typy_pojazdów` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -48,4 +30,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-12 14:30:57
+-- Dump completed on 2020-06-13 15:28:45
